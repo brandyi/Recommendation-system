ed6d67f9f9e63d866a04274d4aae3704f7935200
Marcel Brand <marcelbrand555@gmail.com>
2025-03-09 16:58:04 +0100

    fix cors

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,7 +1,7 @@
-const whitelist = ['http://localhost:5173'];
+const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
