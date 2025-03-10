9b1947be909ba3c7d0e5620477e719cfb796dad2
Marcel Brand <marcelbrand555@gmail.com>
2025-03-10 20:07:24 +0100

    modification because of CORS error

---
+++ backend/config/allowedOrigins.js
@@ -0,0 +1,3 @@
+const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
+
+export default whitelist;
--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,7 +1,8 @@
-const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
+import allowedOrigins from './allowedOrigins.js';
+
 const corsOptions = {
   origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true)
     } else {
       callback(new Error('Not allowed by CORS'))
