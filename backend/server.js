976701ef198ed00f8178244f2b7abbc8d11ae24e
Marcel Brand <marcelbrand555@gmail.com>
2025-03-10 20:09:35 +0100

    add changes and functionalities

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import corsOptions from './config/corsOptions.js';
 import registerRouter from './routes/register.js';
 import loginRouter from './routes/auth.js';
+import verifyJWT from './middleware/verifyJWT.js';
+import refreshRouter from './routes/refresh.js';
+import logoutRouter from './routes/logout.js';
+import credentials from './middleware/credentials.js';
+
 
 const app = express();
 const port = 3000;
 
-
+app.use(credentials);
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/register', registerRouter);
 app.use('/auth', loginRouter);
+app.use('/refresh', refreshRouter);
+app.use('/logout', logoutRouter);
+app.use(verifyJWT);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
