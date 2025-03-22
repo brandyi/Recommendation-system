4bbb95a8419f2ca9be69f9365543d8eda7fdbd9c
Marcel Brand <marcelbrand555@gmail.com>
2025-03-22 19:08:12 +0100

    route handle

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import Login from './pages/Login';
-import Register from './pages/Register';
+import Questionnaire from './components/Questionnaire';
+import Login from './components/Login';
+import Register from './components/Register';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
 
 function App() {
   return (
   <main className='App'>
     <Router>
       <Routes>
-        <Route path='/' element={<Login />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path='/survey' element={<Questionnaire />} />
       </Routes>
     </Router>
   </main>
