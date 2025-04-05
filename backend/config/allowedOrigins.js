ac022ded76014f33a228c3b368d9ee74901c0cea
Marcel Brand <marcelbrand555@gmail.com>
2025-04-05 14:29:27 +0200

    add new vite port

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,3 @@
-const whitelist = ['http://localhost:5173', 'http://localhost:3000'];
+const whitelist = ['http://localhost:5173', 'http://localhost:3000', 'http://localhost:5174'];
 
 export default whitelist;
