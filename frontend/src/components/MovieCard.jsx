82681c62ac4a64e320b925ed16030c6ba872dc7a
Marcel Brand <marcelbrand555@gmail.com>
2025-05-02 21:44:54 +0200

    fix center img

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 import { FaHeart, FaRegHeart } from "react-icons/fa";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import axios from "axios";
@@ -11,35 +11,34 @@ const MovieCard = ({ movie, onLike }) => {
 
   const axiosPrivate = useAxiosPrivate();
 
-  
-
   useEffect(() => {
-
     setImgSrc(null);
     const fetchMovieImage = async () => {
       try {
-        const responseBackend = await axiosPrivate.get(`/movies/${movie.itemID}`);
+        const responseBackend = await axiosPrivate.get(
+          `/movies/${movie.itemID}`
+        );
         const tmdbID = responseBackend.data;
 
         console.log("Movie ID from backend:", tmdbID);
 
         const responseAPI = await axios.get(
-          `https://api.themoviedb.org/3/movie/${tmdbID}?language=en-US`, { 
+          `https://api.themoviedb.org/3/movie/${tmdbID}?language=en-US`,
+          {
             headers: {
-              accept: 'application/json',
-              Authorization: 'Bearer ' + API_TOKEN,
-            }
+              accept: "application/json",
+              Authorization: "Bearer " + API_TOKEN,
+            },
           }
         );
 
         const imagePath = responseAPI.data.poster_path;
-        
+
         if (imagePath) {
           setImgSrc(`https://image.tmdb.org/t/p/original${imagePath}`);
         } else {
           setImgSrc(null);
         }
-
       } catch (error) {
         console.error("Error fetching movie image:", error);
       }
@@ -62,15 +61,19 @@ const MovieCard = ({ movie, onLike }) => {
       : movie.genres || [];
 
   return (
-    <div className="bg-white rounded-lg overflow-hidden shadow-xl mx-auto 
-    h-auto sm:h-4/5 md:h-3/4 lg:h-3/5 xl:h-2/3">
+    <div
+      className="bg-white rounded-lg overflow-hidden shadow-xl mx-auto 
+    h-auto sm:h-4/5 md:h-3/4 lg:h-3/5 xl:h-2/3"
+    >
       <div className="aspect-[2/3] bg-gray-200 flex flex-col items-center justify-center text-center text-gray-500 max-h-[60vh]">
-      {imgSrc ? (
-          <img 
-            src={imgSrc} 
-            alt={movie.title} 
-            className="w-full h-full object-contain"
-          />
+        {imgSrc ? (
+          <div className="w-full h-full flex items-center justify-center overflow-hidden">
+            <img
+              src={imgSrc}
+              alt={movie.title}
+              className="w-full h-full object-contain"
+            />
+          </div>
         ) : (
           <>
             <svg
@@ -87,8 +90,12 @@ const MovieCard = ({ movie, onLike }) => {
                 d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
               />
             </svg>
-            <p className="text-sm font-medium text-gray-500">Obrázok nedostupný</p>
-            <p className="text-base font-semibold mt-1 text-gray-600">{movie.title}</p>
+            <p className="text-sm font-medium text-gray-500">
+              Obrázok nedostupný
+            </p>
+            <p className="text-base font-semibold mt-1 text-gray-600">
+              {movie.title}
+            </p>
           </>
         )}
       </div>
